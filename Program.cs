419d2bfaef11dc3fd246767dfb8e430571d2d987
RBREHAM <raphael.breham@viacesi.fr>
2023-03-06 14:08:15 +0100

    Cas Pratique

--- Program.cs
+++ Program.cs
@@ -4,16 +4,35 @@
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("yearly salary : ");
-        string salary = Console.ReadLine();
-        Console.WriteLine("taxes per cent : ");
-        string taxes = Console.ReadLine();
-        Show(float.Parse(salary), float.Parse(taxes));
+        bool salaryIsInt = int.TryParse(Console.ReadLine(), out int salary);
+
+        if(salaryIsInt)
+        {
+            Console.WriteLine("taxes per cent");
+            bool taxesIsInt = int.TryParse(Console.ReadLine(), out int taxes);
+
+            if (salary > 100000)
+            {
+                Console.WriteLine("Excusez moi Mr Besos");
+            }
+            else if (salary > 50000)
+            {
+                Console.WriteLine("Faires des dons pour reduire ses impots");
+            }
+            else if(salary < 1500*12)
+            {
+                Console.WriteLine("Tu es alternant, dommage");
+            }
+            else if(30000 < salary && salary < 40000)
+            {
+                Console.WriteLine("Viens dev au CESI !");
+            }
+        }
     }
 
     static void Show(float salary, float taxes)
     {
         Console.WriteLine("salary : " + salary / 12 + "€");
         Console.WriteLine("after taxes : " + ((salary / 12) - ((taxes / 100) * (salary / 12))) + "€");
-
     }
 }
