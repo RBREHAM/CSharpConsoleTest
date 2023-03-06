364d3294be7153d3312a68dcd305fc00791f6155
RBREHAM <raphael.breham@viacesi.fr>
2023-03-06 16:19:01 +0100

    feat(introduction) : Derniere etape finie

--- Program.cs
+++ Program.cs
@@ -1,18 +1,54 @@
-﻿class Program
+﻿using System.Collections;
+using System.Globalization;
+
+class Program
 {
     static void Main(string[] args)
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.WriteLine("Salaire a l'annee : ");
-        bool salaryIsInt = int.TryParse(Console.ReadLine(), out int salary);
-        if(salaryIsInt)
+
+        string[] calendrier = new string[12];
+        calendrier[0] = "Janvier";
+        calendrier[1] = "Fevrier";
+        calendrier[2] = "Mars";
+        calendrier[3] = "Avril";
+        calendrier[4] = "Mai";
+        calendrier[5] = "Juin";
+        calendrier[6] = "Juillet";
+        calendrier[7] = "Aout";
+        calendrier[8] = "Septembre";
+        calendrier[9] = "Octobre";
+        calendrier[10] = "Novembre";
+        calendrier[11] = "Decembre";
+
+        Console.WriteLine("Salaire annuel : ");
+        double.TryParse(Console.ReadLine(), out double salary);
+        Console.WriteLine("taxe : ");
+        double.TryParse(Console.ReadLine(), out double taxe);
+
+        foreach (string i in calendrier)
         {
-            ShowSalary(salary);
+            if (i == "Aout")
+            {
+                Console.WriteLine("Entreprise fermée pour le mois d'Aout");
+            }
+            else
+            {
+                double salaireNet = CalculSalary(salary, taxe);
+                if (i == "Decembre")
+                {
+                    salaireNet = salaireNet + (0.1 * salaireNet);
+                }
+                Console.WriteLine("Salaire net du mois de " + i + " : " + salaireNet + "€");
+            }
         }
-        else
-        {
-            Console.WriteLine("Salaire non valide !");
-        }
+    }
+
+
+    static double CalculSalary(double salary, double taxes)
+    {
+        double salary2 = (salary/12) - (taxes/100 * (salary/12));
+        return salary2;
     }
 
     static void ShowSalary(int salary)
