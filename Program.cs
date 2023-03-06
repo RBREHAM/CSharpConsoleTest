d248480c9fb5e425452cc25ef370a8e63d180513
RBREHAM <raphael.breham@viacesi.fr>
2023-03-06 14:21:30 +0100

    feat(introduction) : Salaire mis en une fonction

--- Program.cs
+++ Program.cs
@@ -3,35 +3,42 @@
     static void Main(string[] args)
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.WriteLine("yearly salary : ");
+        Console.WriteLine("Salaire a l'annee : ");
         bool salaryIsInt = int.TryParse(Console.ReadLine(), out int salary);
         if(salaryIsInt)
         {
-            Console.WriteLine("taxes per cent");
-            bool taxesIsInt = int.TryParse(Console.ReadLine(), out int taxes);
-
-            if (salary > 100000)
-            {
-                Console.WriteLine("Excusez moi Mr Besos");
-            }
-            else if (salary > 50000)
-            {
-                Console.WriteLine("Faires des dons pour reduire ses impots");
-            }
-            else if(salary < 1500*12)
-            {
-                Console.WriteLine("Tu es alternant, dommage");
-            }
-            else if(30000 < salary && salary < 40000)
-            {
-                Console.WriteLine("Viens dev au CESI !");
-            }
+            ShowSalary(salary);
+        }
+        else
+        {
+            Console.WriteLine("Salaire non valide !");
         }
     }
 
-    static void Show(float salary, float taxes)
+    static void ShowSalary(int salary)
     {
-        Console.WriteLine("salary : " + salary / 12 + "€");
-        Console.WriteLine("after taxes : " + ((salary / 12) - ((taxes / 100) * (salary / 12))) + "€");
+        Console.WriteLine("taxes pourcent");
+        bool taxesIsInt = int.TryParse(Console.ReadLine(), out int taxes);
+
+        if (salary > 100000)
+        {
+            Console.WriteLine("Excusez moi Mr Besos");
+        }
+        else if (salary > 50000)
+        {
+            Console.WriteLine("Faires des dons pour reduire ses impots");
+        }
+        else if (salary < 1500 * 12)
+        {
+            Console.WriteLine("Tu es alternant, dommage");
+        }
+        else if (30000 < salary && salary < 40000)
+        {
+            Console.WriteLine("Viens dev au CESI !");
+        }
+        else
+        {
+            Console.WriteLine("Salaire normal...");
+        }
     }
 }
