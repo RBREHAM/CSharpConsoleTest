c3dc2f27ccd1d23b13f19cd04f1837065363be4f
RBREHAM <raphael.breham@viacesi.fr>
2023-03-07 16:23:59 +0100

    feat(intests) : Backup from "classes"

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Globalization;
+﻿using CSharpConsoleTest.Models;
 
 class Program
 {
@@ -7,48 +6,76 @@ class Program
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+        User user1 = new User(1, "Raphael", "Breham", 19, 1500, 20);
+
+        Console.WriteLine(user1.Id);
+        Console.WriteLine(user1.FirstName);
+        Console.WriteLine(user1.LastName);
+        Console.WriteLine(user1.Old);
+        Console.WriteLine(user1.Salary);
+        Console.WriteLine(user1.Tax);
+
+        User user2 = new(2, "0", "0", 0, 0, 0);
+        Console.WriteLine("First name : ");
+        user2.FirstName = Console.ReadLine();
+        Console.WriteLine("Last name : ");
+        user2.LastName = Console.ReadLine();
+        Console.WriteLine("Age : ");
+        user2.Old = int.Parse(Console.ReadLine());
+        Console.WriteLine("Yearly salary : ");
+        user2.Salary = int.Parse(Console.ReadLine());
+        Console.WriteLine("Taxes : ");
+        user2.Tax = int.Parse(Console.ReadLine());
+
+
+
         double salary = 0;
 
-        string[] calendrier = new string[] { "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre" };
+        string[] months = new string[] { "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre" };
 
         try
         {
-            Console.WriteLine("Salaire annuel : ");
-            salary = double.Parse(Console.ReadLine());
+            salary = user2.Salary;
         }
-        catch(FormatException)
+        catch (FormatException)
         {
             Console.WriteLine("Mauvais format !");
         }
-        
-        Console.WriteLine("taxe : ");
-        double.TryParse(Console.ReadLine(), out double taxe);
-        Console.WriteLine("taxe de Noel : ");
+
+        double taxe = user2.Tax;
+        Console.WriteLine("Xmas prime : ");
         double.TryParse(Console.ReadLine(), out double Xmasprime);
 
 
-        foreach (string i in calendrier)
+        foreach (string month in months)
         {
-            if (i == "Aout")
+            if (month == "Aout")
             {
                 Console.WriteLine("Entreprise fermée pour le mois d'Aout");
             }
             else
             {
-                double salaireNet = CalculSalary(salary, taxe);
-                if (i == "Decembre")
+                double netSalary = CalculSalary(salary, taxe);
+                if (month == "Decembre")
                 {
-                    salaireNet = salaireNet + ((Xmasprime/100) * salaireNet);
+                    netSalary = netSalary + ((Xmasprime / 100) * netSalary);
                 }
-                Console.WriteLine("Salaire net du mois de " + i + " : " + salaireNet + "€");
+                Console.WriteLine("Salaire net du mois de " + month + " de " + user2.FirstName + ": " + netSalary + "€");
             }
         }
+
+        Console.WriteLine(user1.Id);
+        Console.WriteLine(user1.FirstName);
+        Console.WriteLine(user1.LastName);
+        Console.WriteLine(user1.Old);
+        Console.WriteLine(user1.Salary);
+        Console.WriteLine(user1.Tax);
     }
 
 
     static double CalculSalary(double salary, double taxes)
     {
-        double salary2 = (salary/12) - (taxes/100 * (salary/12));
+        double salary2 = (salary / 12) - (taxes / 100 * (salary / 12));
         return salary2;
     }
 
