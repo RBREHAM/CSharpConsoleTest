ed10a797f172a23b276ddbbe61a8494caf83b4ab
RBREHAM <raphael.breham@viacesi.fr>
2023-03-07 16:21:02 +0100

    feat(intests) : Fail backup file

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpConsoleTest.Models
+{
+    public class User
+    {
+        public User(int id, string firstName, string lastName, int old, int salary, int tax)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Old = old;
+            this.Salary = salary;
+            this.Tax = tax;
+        } 
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Old { get; set; }
+        public int Salary { get; set; }
+        public int Tax { get; set; }
+    }
+}
